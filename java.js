1d581c4517cf4c44102e4e2db82807785322d419
Zoe Medina <zoemedina@live.com>
2018-07-10 11:48:00 -0700

    First Upload A Website

---
+++ java.js
@@ -0,0 +1,18 @@
+
+var timer = 0;
+function increase(timer) {
+  timer = timer + 1 ;
+  return timer;
+}
+function title() {
+  if (timer % 2 = 0) {
+    <script>
+    document.getElementById("intro").innerHTML = "Zoe Medina";
+    </script>
+  } else {
+    <script>
+    document.getElementById("intro").innerHTML = "Welcome to my website";
+    </script>
+  }
+  increase(timer);
+}
